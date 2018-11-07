6d1d25be917d47c18ebbb8670d30ef2ae49f1bba
veronica ramos <veronicaramos@veronica-ramoss-iMac-2.local>
2018-11-07 15:11:15 -0800

    add code that asks the user questions

---
+++ cin.cpp
@@ -0,0 +1,28 @@
+#include "main.hpp"
+int main(){
+
+  string name = " ";
+  string age = " ";
+  string yes = " ";
+  string no = " ";
+  //int yearBorn = " ";
+  //int counryBorn = " ";
+
+  cout<<"Enter Name";
+  cin>>name;
+  cout<<"Enter age";
+  cin>>age;
+  cout<<"This is the information you entered"<<name<<age;
+
+  cout<<"is the information you entered correct (yes/no)?";
+  cin>>yes;
+  if(yes == "yes"){
+    cout<<"thanks for your cubmission";
+  }else{
+    cout<<"fill out your information again";
+  }
+  return 0;
+
+
+
+}
